6c0072464ba936bb0dbdbc5034f69ccb840b31d1
PRATIK  MONDAL <99479145+pratikm0007@users.noreply.github.com>
2022-02-28 01:01:04 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Co-ordinates value for the Length of Line");
+            UC_1CalculateTheLength.LenghtBtwnTwoPoint();
+
+            Console.WriteLine("Enter Co-ordinates value to Check Equality of two Li>
+            UC_2CheckEquality.CheckEqualityOfTwoLines();
+
+            Console.WriteLine("Enter Co-ordinates value to Check Comparison of two >
+            UC_3CompareTwoLines.CompareTwoLineForGrtEqlLessThan();
+
+        }
+    }
+
+}
+
